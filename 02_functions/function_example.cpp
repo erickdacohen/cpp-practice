512e2f3e63271cd80cd1352de10272ef9cd8203f
Erick Cohen <erickcohen@Ericks-MacBook-Air.local>
2022-11-23 13:58:11 -0500

    Initial commit

---
+++ 02_functions/function_example.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+
+int addNumbers(int first_number, int second_number) {
+    int sum = first_number + second_number;
+    return sum;
+}
+
+int main()
+{
+    int first_number = 10;
+    int second_number = 13;
+
+    int sum = addNumbers(first_number, second_number);
+    
+    std::cout << "The sum is: " << sum << std::endl;
+    std::cout << addNumbers(99, -9) << std::endl;
+    return 0;
+}
---
+++ 03_print_messages/print_messages.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    // Initialize variables
+    // int age;
+    // std::string name;
+
+    // // Prompt user for input
+    // std::cout << "What is your name? " << std::endl;
+    // std::cin >> name;
+
+    // std::cout << "How old are you? " << std::endl;
+    // std::cin >> age;
+
+    // Print bytes using sizeof() 
+
+    int rand_num = 10;
+    std::cout << "The size of the int variable is: " << sizeof(rand_num) << std::endl;
+
+    std::cout << "Enter where you are from: " << std::endl;
+    std::string city;
+    std::getline(std::cin, city);
+    
+    
+    // std::cout << "Hello " << name << ". You are " << age << " years old." << std::endl;
+    std::cout << "You are from " << city << std::endl;
+    
+    return 0;
+}
