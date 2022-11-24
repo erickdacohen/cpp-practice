bc5bf2c3391f4f1c9cda70a1ab0fd46ba57fa1cc
Erick Cohen <erickcohen@Ericks-MacBook-Air.local>
2022-11-24 08:14:18 -0500

    Adds 08 and 09

---
+++ 08_basic_operations/basic_operations.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{
+    // Initialize values
+    float a = 5.0;
+    float b = 10.0;
+
+    std::cout << "a = 5.0 and b = 10.0" << std::endl;
+
+    // Addition
+    std::cout << "a + b = " << a + b << std::endl;
+    // Subtraction
+    std::cout << "a - b = " << a - b << std::endl;
+    // Multiplication
+    std::cout << "a * b = " << a * b << std::endl;
+    // Division
+    std::cout << "a / b = " << a / b << std::endl;
+
+    // Modulus example 
+    int c = 6;
+    int d = 11;
+    std::cout << "Modulus operator only works with integers. So we will use c = 6 and d = 11 to find\nc % d = " << c % d << std::endl;
+
+    return 0;
+}
---
+++ 09_output_formatting/formatting.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+    // `std::flush` sends the output buffer to the final destination (terminal)
+    std::cout << "This is a message.\n" << std::endl << std::flush;
+    // Right after this line, the message above is sent to the terminal
+
+    std::cout << "-------------------------------" << std::endl;
+
+    // setting width
+    std::cout << "Unformated table\n" << std::endl;
+    std::cout << "LeBron James" << " 6" << std::endl;
+    std::cout << "Dwayne Wade" << " 3" << std::endl;
+    std::cout << "Udonis Haslem" << " 40" << std::endl;
+    std::cout << "Chris Bosh" << " 1" << std::endl;
+    std::cout << "Ray Allen" << " 34" << std::endl;
+
+    std::cout << "\nFormatted table\n" << std::endl;
+    
+    int col_width = 15;
+    int col_width_2 = 4;
+    // left justify columns
+    std::cout << std::left;
+    std::cout << std::setw(col_width) << "LeBron James"  << std::setw(col_width_2) << " 6" << std::endl;
+    std::cout << std::setw(col_width) << "Dwayne Wade"  << std::setw(col_width_2) << " 3" << std::endl;
+    std::cout << std::setw(col_width) << "Udonis Haslem"  << std::setw(col_width_2) << " 40" << std::endl;
+    std::cout << std::setw(col_width) << "Chris Bosh"  << std::setw(col_width_2) << " 1" << std::endl;
+    std::cout << std::setw(col_width) << "Ray Allen"  << std::setw(col_width_2) << " 34" << std::endl;
+
+
+    return 0;
+}
