160fc93862d2b535bf7ca4ccbbe1084bf26118ba
Erick Cohen <erickcohen@Ericks-MacBook-Air.local>
2022-11-24 17:52:18 -0500

    adds gitignore and modifies loops

--- 01_loop/loop.cpp
+++ 01_loop/loop.cpp
@@ -6,5 +6,26 @@ int main()
     {
         std::cout << "i is: " << i << std::endl;
     }
+
+    std::cout << "\n---------------------\nwhile loops" << std::endl;
+
+    size_t COUNT = 20;
+    size_t i = 0;
+    while (i < COUNT)
+    {
+        std::cout << i << ". I love C++" << std::endl;
+        ++i;
+    }
+
+    std::cout << "\n---------------------\ndo while loops\n"
+              << std::endl;
+
+    size_t j = 21;
+    do
+    {
+        std::cout << "drink" << std::endl;
+        ++j;
+    } while (j < COUNT);
+
     return 0;
 }
